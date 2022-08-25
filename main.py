f0859dae10ca8be4846c10a9d91718fc7916063f
Administrador_local <gretzlaff13@gmail.com>
2022-08-25 16:40:47 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from settings import *
+
+class Game:
+    def __init__(self):
+        #setup geral
+        pygame.init()
+        self.screen = pygame.display.set_mode(width, heigth)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type ==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill("black")
+            pygame.display.update()
+            self.clock.tick(fps)
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
