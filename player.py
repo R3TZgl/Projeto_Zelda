f41a78e9d9633f580be9517355d5d0111b2660b4
Gabriel Retzlaff <gretzlaff13@gmail.com>
2024-03-18 22:03:09 -0300

    start

--- player.py
+++ player.py
@@ -4,5 +4,26 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("../graphics/test/Player.png").convert_alpha()
+        self.image = pygame.image.load("level_graphics\graphics\player\down\down_0.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        
+        self.direction = pygame.math.Vector2()
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+            
+        if keys[pygame.K_RIGHT]:
+            self.direction.y = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+            
+    def update(self)
--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("../graphics/test/rock.png").convert_alpha()
+        self.image = pygame.image.load("level_graphics\graphics\objects/09.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
